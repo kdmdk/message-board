cc5ff2350e25abd910b48eaf7cb3c1f639dc6907
kodama t <hzmd8274@gmail.com>
2017-03-09 10:17:19 +0000

    btn rebirth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'messages/index'
 
   root 'messages#index'
-  resource :messages , expect: [:index, :new]
+  resources :messages , expect: [:index, :new]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
